f556bded25abf066dd4ddbbcbb64f863b2a25694
flyrk <simon_xiameng@126.com>
2017-09-04 10:59:24 +0800

    Add keydown eventlistener

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 MPC.log.writeRaw('This is a log message');
+
+MPC.addEvent(document, 'keydown', function(W3CEvent) {
+  var key = MPC.getKeyPressed(W3CEvent);
+  MPC.log.write(key.code + ':' + key.value);
+});
